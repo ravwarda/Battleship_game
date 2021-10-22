3e5121968f44e6878e279abc64feeec155827d83
ravwarda <73530034+ravwarda@users.noreply.github.com>
2021-10-22 16:45:30 +0200

    ship set up

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const literki = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 const poleGry = document.querySelector("#player");
+let dlStatku = 5;
 
-function strzal(i,j) {
-    console.log("paf" + i + j);
+function strzal(i, j) {
+    for (let d = 0; d < dlStatku; d++) {
+        if (j + d < 11) {
+            kratki[i][j+d].czyStatek = true;
+            kratki[i][j+d].pole.style.backgroundColor = "rgb(80, 80, 80)";
+            dlStatku--;
+        }
+        if (dlStatku <= 1) {
+            
+        }
+    }
+}
+
+function ustawKratke(znak, i, j) {
+        kratki[i][j] = document.createElement("div");
+        kratki[i][j].innerText = znak;
+        poleGry.appendChild(kratki[i][j]);
 }
 
 class kratka {
 
     czyTrafione = false;
     czyStatek = false;
+    czyStatekObok = false;
 
     constructor(pole) {
         this.pole = pole;
@@ -18,26 +35,21 @@ class kratka {
 const kratki = [];
 for (let i = 0; i < 11; i++) {
     for (let j = 0; j < 11; j++) {
-        kratki[i, j] = new kratka(document.createElement("div"));
         if (i == 0 && j == 0) {
-            kratki[0, j] = document.createElement("div");
-            kratki[0, 0].innerText = "/";
-            poleGry.appendChild(kratki[i, j]);
-        }
-        else if (i == 0) {
-            kratki[0, j] = document.createElement("div");
-            kratki[0, j].innerText = literki[j - 1];
-            poleGry.appendChild(kratki[i, j]);
+            kratki[0] = [];
+            ustawKratke("\\",i,j);
         }
         else if (j == 0) {
-            kratki[i, 0] = document.createElement("div");
-            kratki[i, 0].innerText = i;
-            poleGry.appendChild(kratki[i, j]);
+            kratki[i] = [];
+            ustawKratke(i,i,j);
+        }
+        else if (i == 0) {
+            ustawKratke(literki[j - 1],i,j);
         }
         else {
-            kratki[i, j] = new kratka(document.createElement("div"));
-            poleGry.appendChild(kratki[i, j].pole);
-            kratki[i, j].pole.addEventListener("click", function() {
+            kratki[i][j] = new kratka(document.createElement("div"));
+            poleGry.appendChild(kratki[i][j].pole);
+            kratki[i][j].pole.addEventListener("click", function() {
                 strzal(i,j);
             });
         }
